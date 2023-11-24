9ca61e4eb930e27427d4ddfa1b32314236f0d963
Lucas Auada Braga <127341765+Kaliedy@users.noreply.github.com>
2023-11-24 12:44:42 -0300

    Update escada.c

--- Proj2/escada.c
+++ Proj2/escada.c
@@ -4,11 +4,42 @@
 #define MAX_RIDERS 10000
 
 typedef struct {
-    int t; // Tempo de chegada do usuário
-    int d; // Direção do usuário: 0 para esquerda, 1 para direita
+    int t;
+    int d;
 } rider;
 
-// Função para ler os dados do arquivo
+int solve(rider* riders, int n) {
+    int moment = riders[0].t + 10;
+    int waiting = 0;
+    int direction = riders[0].d;
+
+    for (int i = 1; i < n; i++) {
+        if (riders[i].t <= moment) {
+            if (riders[i].d == direction) {
+                moment = riders[i].t + 10;
+            } else {
+                waiting = 1;
+            }
+        } else {
+            if (waiting) {
+                moment += 10;
+                direction = (direction == 1) ? 0 : 1;
+                i--;
+            } else {
+                moment = riders[i].t + 10;
+                direction = riders[i].d;
+            }
+            waiting = 0;
+        }
+    }
+
+    if (waiting) {
+        moment += 10;
+    }
+
+    return moment;
+}
+
 void readData(char* filePath, rider* riders, int* n) {
     FILE* file = fopen(filePath, "r");
     if (file == NULL) {
@@ -24,60 +55,35 @@ void readData(char* filePath, rider* riders, int* n) {
     fclose(file);
 }
 
-// Função que simula o funcionamento da escada rolante
-int escalator(rider* riders, int n) {
-    int moment = 0;
-    int direction = -1;
-    int waiting = 0;
-
-    for (int i = 0; i < n; i++) {
-        if (riders[i].t < moment) {
-            // Usuário chegou antes do momento atual
-            if (riders[i].d == direction) {
-                moment = riders[i].t + 10;
-            } else {
-                waiting = 1; // Alguém está esperando na fila
-            }
-        } else {
-            // Usuário chegou no momento ou depois do momento atual
-            if (waiting) {
-                // Se alguém estava esperando, incrementa 10 no tempo e reseta direção
-                moment += 10;
-                direction = -1;
-                i--; // Reprocessa a mesma pessoa
-            } else {
-                // Se não havia ninguém esperando, atualiza o tempo e direção
-                moment = riders[i].t + 10;
-                direction = riders[i].d;
-            }
-            waiting = 0; // Reseta a flag de espera
-        }
+void readExpectedOutput(char* filePath, int* expected) {
+    FILE* file = fopen(filePath, "r");
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo %s.\n", filePath);
+        exit(1);
     }
 
-    if (waiting) {
-        moment += 10; // Se há alguém esperando no final, incrementa 10 no tempo
-    }
-
-    return moment;
+    fscanf(file, "%d", expected);
+    fclose(file);
 }
 
 int main() {
-    // Loop para processar os arquivos de 1 a 58
     for (int fileNumber = 1; fileNumber <= 58; fileNumber++) {
-        char fileName[20];
-        sprintf(fileName, "input/E_%d", fileNumber);
+        char inputFileName[20];
+        char outputFileName[20];
+
+        sprintf(inputFileName, "input/E_%d", fileNumber);
+        sprintf(outputFileName, "output/E_%d", fileNumber);
 
         rider riders[MAX_RIDERS];
         int n;
+        int expectedOutput;
 
-        // Chama a função para ler os dados do arquivo
-        readData(fileName, riders, &n);
+        readData(inputFileName, riders, &n);
+        readExpectedOutput(outputFileName, &expectedOutput);
 
-        // Chama a função para simular o funcionamento da escada rolante
-        int lastMoment = escalator(riders, n);
+        int lastMoment = solve(riders, n);
 
-        // Imprime o resultado para o arquivo específico
-        printf("Resultado para o arquivo %s: O último momento em que a escada para é %d\n", fileName, lastMoment);
+        printf("Para %s: Último momento em que a escada para: %d. Resposta esperada: %d\n", inputFileName, lastMoment, expectedOutput);
     }
 
     return 0;
